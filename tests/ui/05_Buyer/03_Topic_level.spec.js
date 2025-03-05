b4eab8b08b80aaa8cd5c2691e73aacec7a52020a
Nitesh Agarwal <nitesh.agarwal@kreeti.com>
2025-03-05 22:44:47 +0530

    Added Ui Listings Topic and Step_Group Level test cases

---
+++ tests/ui/05_Buyer/03_Topic_level.spec.js
@@ -0,0 +1,152 @@
+import { test, expect } from '@playwright/test';
+import { ListingPage } from '../../../pages/listingsPage';
+import { LoginPage } from "../../../pages/loginPage";
+import { ValidTestData } from '../../data/SignUpData';
+import { getData } from '../../utils/apiHelper';
+import path from 'path';
+import fs from 'fs';
+import { ProjectListings } from '../../../pages/projectListingPage';
+import { validateStepGroupVisiblity, validateTopicVisiblity } from '../../utils/listingsProjectHelper';
+
+const ViewDataPath = path.join(__dirname, '..', '..', 'data', 'view-data.json');
+const viewData = JSON.parse(fs.readFileSync(ViewDataPath, 'utf-8'));
+
+
+// Validate the visibility of the topic and step group for unauthenticated users
+test.describe('Topic and Step Group Visibility for Unauthenticated Users', () => {
+
+  test.beforeEach(async ({ page, baseURL }) => {
+  
+    const listingPage = new ListingPage(page);
+    await page.goto(`${baseURL}/listings`);
+
+    // Click on first project to navigate to project details
+    const projectTitle = await listingPage.projectItemCardContentMainTitle();
+    await expect(projectTitle).toBeVisible();
+    await projectTitle.click();
+    await page.waitForURL('**/overview');
+  });
+
+  // Loop through each topic and validate visibility
+  for (const topic of viewData.topics) {
+
+    if(topic.label == 'Updates') continue;
+    // Validate topic visibility
+    test(`Verify Topic is Visible: ${topic.label} for Unauthenticated Users`, async ({ page }) => {
+      const errors = [];
+      const projectListings = new ProjectListings(page);
+     
+      await validateTopicVisiblity(projectListings, topic, errors);
+
+      if (errors.length > 0) {
+        throw new Error(`Validation errors found:\n${errors.join('\n')}`);
+      }
+    });
+
+
+    // Validate step group visibility
+    if (topic.step_groups) {
+
+      // Check if the step group has a label
+      const hasField = topic.step_groups.some(group => group.label !== '');
+
+      // Validate step group visibility
+      test(`Verify Step Groups in Topic: ${topic.label} for Unauthenticated Users`, async ({ page }) => {
+        
+        // Skip the test if no step group label is found
+        test.skip(!hasField, `No Step Group Label found in ${topic.label}`);
+        
+        const errors = [];
+        const projectListings = new ProjectListings(page);
+        const topicLabel = await projectListings.topicName(topic.label);
+        await topicLabel.click();
+        
+        // Loop through each step group and validate visibility
+        for (const stepGroup of topic.step_groups) {
+          if (!stepGroup?.steps) continue;
+          await validateStepGroupVisiblity(projectListings, stepGroup, errors);
+        }
+
+        if (errors.length > 0) {
+          throw new Error(`Validation errors found:\n${errors.join('\n')}`);
+        }
+      });
+    }
+  }
+
+});
+
+// validate the visibility of the topic and step group for authenticated users
+test.describe('Topic and StepGroup Visibility for Authenticated Users', () => {
+
+  const { newEmail } = getData('UI');
+
+  test.beforeEach(async ({ page, baseURL }) => {
+    const loginPage = new LoginPage(page, baseURL);
+     const listingPage = new ListingPage(page);
+
+    // Navigate to the login page and Login
+    await loginPage.navigate();
+    await loginPage.login(newEmail, ValidTestData.newPassword);
+    await page.waitForURL('**/projects');
+    const ListingsButton = await listingPage.listings();
+    await expect(ListingsButton).toBeVisible();
+    await ListingsButton.click();
+    await page.waitForURL('**/listings');
+
+    // Click on project to navigate to project details
+    const projectTitle = await listingPage.projectItemCardContentMainTitle();
+    await expect(projectTitle).toBeVisible();
+    await projectTitle.click();
+    await page.waitForURL('**/overview');
+  });
+
+  // Loop through each topic and validate visibility
+  for (const topic of viewData.topics) {
+
+    // Validate topic visibility
+    test(`Verify Topic is Visible: ${topic.label} for Authenticated Users`, async ({ page }) => {
+      const errors = [];
+      const projectListings = new ProjectListings(page);
+      
+      await validateTopicVisiblity(projectListings, topic, errors);
+
+      if (errors.length > 0) {
+        throw new Error(`Validation errors found:\n${errors.join('\n')}`);
+      }
+    });
+
+
+    // Validate step group visibility
+    if (topic.step_groups) {
+
+      // Check if the step group has a label
+      const hasField = topic.step_groups.some(group => group.label !== '');
+
+      // Validate step group visibility
+      test(`Verify Step Groups in Topic: ${topic.label} for Authenticated Users`, async ({ page }) => {
+        
+        // Skip the test if no step group label is found
+        test.skip(!hasField, `No Step Group Label found in ${topic.label}`);
+        
+        const errors = [];
+        const projectListings = new ProjectListings(page);
+        
+        const topicLabel = await projectListings.topicName(topic.label);
+        await topicLabel.click();
+
+        // Loop through each step group and validate visibility
+        for (const stepGroup of topic.step_groups) {
+          if (!stepGroup?.steps) continue;
+          await validateStepGroupVisiblity(projectListings, stepGroup, errors);
+        }
+
+        if (errors.length > 0) {
+          throw new Error(`Validation errors found:\n${errors.join('\n')}`);
+        }
+      });
+
+    }
+  }
+
+});
