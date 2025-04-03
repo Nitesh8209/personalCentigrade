42f904f99f71b4eee8cb66251f67821828ddb6f2
Nitesh Agarwal <nitesh.agarwal@kreeti.com>
2025-04-03 16:50:32 +0530

    Update the SignUp Tests and config file

--- playwright.config.js
+++ playwright.config.js
@@ -23,13 +23,13 @@ module.exports = defineConfig({
   snapshotPathTemplate: 'tests/assets/{arg}{ext}',
   snapshotDir: '__snapshots__',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  // fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 1,
+  retries: process.env.CI ? 1 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 3 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { outputFolder: 'playwright-report' }],
@@ -42,7 +42,6 @@ module.exports = defineConfig({
     baseURL: baseUrl,
     actionTimeout: 60000,
     navigationTimeout: 60000,
-    
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
     screenshot: 'on'
@@ -53,13 +52,173 @@ module.exports = defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] ,
+    //     viewport: { width: 1366, height: 768 },
+    //   },
+    // },
+    // {
+    //   name: 'API',
+    //   testMatch: [ 'api/*/*.spec.js' ],
+    //    use: { ...devices['Desktop Chrome'] ,
+    //     viewport: { width: 1366, height: 768 },
+    //   },
+    //   fullyParallel: false,
+    // },
     {
-      name: 'chromium',
+      name: 'login',
+      testMatch: [ 'ui/01_Login/loginTest.spec.js' ],
+       use: { ...devices['Desktop Chrome'] ,
+        viewport: { width: 1366, height: 768 },
+      },
+      fullyParallel: false,
+    },
+    {
+      name: 'CompleteSignUp',
+      testMatch: [ 'ui/02_SignUp/0_completeSignUp.spec.js'],
       use: { ...devices['Desktop Chrome'] ,
         viewport: { width: 1366, height: 768 },
       },
+      fullyParallel: false,
     },
-
+    {
+      name: 'CreateAccount',
+      testMatch: [ 'ui/02_SignUp/1_CreateAccount.spec.js'],
+      use: { ...devices['Desktop Chrome'] },
+      fullyParallel: false,
+    },
+    {
+      name: 'verify',
+      testMatch: [ 'ui/02_SignUp/2_verify.spec.js'],
+      use: { ...devices['Desktop Chrome'] ,
+        viewport: { width: 1366, height: 768 },
+      },
+      fullyParallel: false,
+    },
+    {
+      name: 'forgotPassword',
+      testMatch: [ 'ui/02_SignUp/3_forgotPassword.spec.js' ],
+      use: { ...devices['Desktop Chrome'] ,
+        viewport: { width: 1366, height: 768 },
+      },
+      fullyParallel: false,
+    },
+    {
+      name: 'invite',
+      testMatch: [ 'ui/02_SignUp/4_invite.spec.js'],
+      use: { ...devices['Desktop Chrome'],
+        viewport: { width: 1366, height: 768 },
+      },
+      fullyParallel: false,
+      dependencies: ['CompleteSignUp'],
+    },
+    {
+      name: 'Settings',
+      testMatch: [ 'ui/03_Settings/1_myAccount.spec.js',
+         'ui/03_Settings/2_organization.spec.js', 'ui/03_Settings/3_team.spec.js'
+        ],
+      use: { ...devices['Desktop Chrome'],
+        viewport: { width: 1366, height: 768 },
+      },
+      fullyParallel: false,
+      dependencies: ['CompleteSignUp'],
+    },
+    {
+      name: 'organizationRole',
+      testMatch: ['ui/03_Settings/4_organizationRole.spec.js' ],
+      use: { ...devices['Desktop Chrome'],
+        viewport: { width: 1366, height: 768 },
+      },
+      fullyParallel: false,
+      dependencies: ['CompleteSignUp'],
+    },
+    {
+      name: 'organizationEdgeCase',
+      testMatch: ['ui/03_Settings/5_edgeCase.spec.js'],
+      use: { ...devices['Desktop Chrome'],
+        viewport: { width: 1366, height: 768 },
+      },
+      fullyParallel: false,
+      dependencies: ['organizationRole'],
+    },
+    {
+      name: 'ProjectPage',
+      testMatch: [ 'ui/04_Project/01_ProjectPage.spec.js'],
+      use: { ...devices['Desktop Chrome'],
+        viewport: { width: 1366, height: 768 },
+      },
+      fullyParallel: false,
+    },
+    {
+      name: 'ProjectOverview',
+      testMatch: [ 'ui/04_Project/02_ProjectOverview.spec.js'],
+      use: { ...devices['Desktop Chrome'],
+        viewport: { width: 1366, height: 768 },
+      },
+      fullyParallel: false,
+      dependencies: ['ProjectPage'],
+    },
+    {
+      name: 'ProjectTopics',
+      testMatch: [ 'ui/04_Project/03_Topic_level.spec.js'],
+      use: { ...devices['Desktop Chrome'],
+        viewport: { width: 1366, height: 768 },
+      },
+      fullyParallel: false,
+      dependencies: ['ProjectPage'],
+    },
+    {
+      name: 'Projectsteps',
+      testMatch: [ 'ui/04_Project/04_Step_level.spec.js'],
+      use: { ...devices['Desktop Chrome'],
+        viewport: { width: 1366, height: 768 },
+      },
+      fullyParallel: false,
+      dependencies: ['ProjectPage'],
+    },
+    {
+      name: 'ProjectFields',
+      testMatch: [ 'ui/04_Project/05_Field_level.spec.js'],
+      use: { ...devices['Desktop Chrome'],
+        viewport: { width: 1366, height: 768 },
+      },
+      fullyParallel: false,
+      dependencies: ['ProjectPage'],
+    },
+    {
+      name: 'ProjectButtons',
+      testMatch: [ 'ui/04_Project/06_Form_discard_changes.spec.js'],
+      use: { ...devices['Desktop Chrome'],
+        viewport: { width: 1366, height: 768 },
+      },
+      fullyParallel: false,
+      dependencies: ['ProjectPage'],
+    },
+    {
+        name: 'PublishProject',
+        testMatch: [ 'ui/04_Project/07_Publish_project.spec.js'],
+        use: { ...devices['Desktop Chrome'],
+          viewport: { width: 1366, height: 768 },
+        },
+        fullyParallel: false,
+    },
+    {
+      name: 'FillRemainingTopics',
+      testMatch: [ 'ui/04_Project/08_Fill_Other_Tiers.spec.js'],
+      use: { ...devices['Desktop Chrome'] },
+      fullyParallel: false,
+      dependencies: ['PublishProject'],
+    },
+      {
+      name: 'ListingsPage',
+      testMatch: [ 'ui/05_Buyer/*.spec.js'],
+      use: { ...devices['Desktop Chrome'],
+        viewport: { width: 1366, height: 768 },
+       },
+      fullyParallel: true,
+      dependencies: ['FillRemainingTopics'],
+    },
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
@@ -98,4 +257,3 @@ module.exports = defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
--- tests/ui/02_SignUp/3_forgotPassword.spec.js
+++ tests/ui/02_SignUp/3_forgotPassword.spec.js
@@ -1,14 +1,10 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from "../../../pages/loginPage";
 import { SignUpPage } from '../../../pages/signUpPage';
-import { getGmailMessages } from '../../utils/signUpHelper';
-import { inValidTestData, ValidTestData } from '../../data/SignUpData';
-import { getData } from '../../utils/apiHelper';
+import { inValidTestData } from '../../data/SignUpData';
 import API_ENDPOINTS from '../../../api/apiEndpoints';
-
+ 
 test.describe('Forgot password Page UI Tests', () => {
-  const { newEmail } = getData('UI');
-  let temporaryPassword;
   let page;
 
   // Setup: Generate a new email before all tests
@@ -64,44 +60,6 @@ test.describe('Forgot password Page UI Tests', () => {
 
   })
 
-  // Test for requesting a password reset with a valid registered email
-  test('Request password reset with a valid registered email', async ({ baseURL }) => {
-
-    const loginPage = new LoginPage(page, baseURL);
-    const signUpPage = new SignUpPage(page, baseURL);
-
-    // Navigate to login page and perform forgot password flow
-    await loginPage.navigate();
-    await signUpPage.forgotPassword();
-    await signUpPage.forgotPasswordEmail(newEmail);
-    await signUpPage.forgotPasswordSend();
-
-    // Wait for the response and verify email message elements
-    const response = await page.waitForResponse(
-      response => response.url().includes(API_ENDPOINTS.resetEmail) && response.status() === 200
-    );
-
-    // Assert email message content
-    const CheckEmail = await signUpPage.checkEmail();
-    const checkEmailMessage = await signUpPage.checkEmailMessage();
-    const loginButton = await signUpPage.backToLoginButton();
-
-    await expect(CheckEmail).toBe('Check your email');
-    await expect(checkEmailMessage).toBe(`If ${newEmail} is registered with Centigrade, we will send an email with instructions on how to reset your password`);
-    await expect(loginButton).toBeVisible();
-
-    // Fetch Gmail message to verify the reset link
-    const { body, subject } = await getGmailMessages(newEmail);
-    expect(subject).toBe('Centigrade password reset request');
-
-    // Extract temporary password and reset link using regex
-    const tempPasswordMatch = body.match(/Your temporary password:\s+([^\s]+)/);
-    const tempPassword = tempPasswordMatch[1];
-    expect(tempPassword).toBeDefined();
-
-    temporaryPassword = tempPassword;
-  })
-
   // Test for requesting a password reset with an invalid email format
   test('Request Password Reset with Invalid Email Format', async ({ baseURL }) => {
 
@@ -131,87 +89,4 @@ test.describe('Forgot password Page UI Tests', () => {
     await expect(page).toHaveURL(`${baseURL}/login`);
   })
 
-
-  test('Successful Password Reset with Mandatory Fields', async ({ baseURL }) => {
-    const signUpPage = new SignUpPage(page, baseURL);
-
-    // Navigate to reset password page
-    await signUpPage.navigateResetPassword(newEmail);
-
-    // Verify the reset password page elements and their content
-    const resetPasswordheading = await signUpPage.resetPasswordheading();
-    const resetPasswordmsg = await signUpPage.resetPasswordmsg();
-    const resetPasswordemail = await signUpPage.resetPasswordemail();
-    const resetPasswordtempPassword = await signUpPage.resetPasswordtempPassword();
-    const resetPasswordtempPasswordbtn = await signUpPage.resetPasswordtempPasswordbtn();
-    const resetPasswordtempHelperText = await signUpPage.resetPasswordtempHelperText();
-    const resetPasswordnewPassword = await signUpPage.resetPasswordnewPassword();
-    const resetPasswordnewPasswordbtn = await signUpPage.resetPasswordnewPasswordbtn();
-    const resetPasswordHelperText = await signUpPage.resetPasswordHelperText();
-    const resetPasswordSubmit = await signUpPage.resetPasswordSubmit();
-
-    // Assert visibility and text of reset password elements
-    expect(resetPasswordheading).toHaveText('Reset Password');
-    expect(resetPasswordmsg).toHaveText('Use the form below to reset your password and recover your account');
-    expect(resetPasswordemail).toHaveAttribute('readonly', '');
-    expect(resetPasswordemail).toHaveValue(newEmail);
-    expect(resetPasswordtempPassword).toBeVisible();
-    expect(resetPasswordtempPasswordbtn).toBeVisible();
-    expect(resetPasswordtempHelperText).toHaveText('This is found in the reset password email that was sent to you');
-    expect(resetPasswordnewPassword).toBeVisible();
-    expect(resetPasswordnewPasswordbtn).toBeVisible();
-    expect(resetPasswordHelperText).toHaveText('Password must be at least 8 characters and contain an uppercase, a lowercase, a number, and a special character');
-    expect(resetPasswordSubmit).toBeVisible();
-
-    // Fill in the form and submit
-    await resetPasswordtempPassword.fill(temporaryPassword);
-    await resetPasswordtempPasswordbtn.click();
-    expect(resetPasswordtempPassword).toHaveAttribute('type', 'text');
-
-    await resetPasswordnewPassword.fill(ValidTestData.newPassword);
-    await resetPasswordtempPasswordbtn.click();
-    expect(resetPasswordnewPassword).toHaveAttribute('type', 'text');
-
-    await resetPasswordSubmit.click();
-
-    // Verify success message after reset
-    const resetPasswordsuccesserrormsg = await signUpPage.resetPasswordsuccesserrormsg();
-    await expect(resetPasswordsuccesserrormsg).toBeVisible();
-    await expect(resetPasswordsuccesserrormsg).toHaveText('Password reset successful');
-
-    // Verify that the confirmation page is shown
-    await expect(page).toHaveURL(`${baseURL}/reset-confirmation`);
-    const resetPasswordsucessheader = await signUpPage.resetPasswordsucessheader();
-    const resetPasswordsucesssubText = await signUpPage.resetPasswordsucesssubText();
-    const backToLoginButton = await signUpPage.backToLoginButton();
-
-    expect(resetPasswordsucessheader).toHaveText('Your password was reset successfully');
-    expect(resetPasswordsucesssubText).toHaveText('Use your new password to log in to your Centigrade account');
-    expect(backToLoginButton).toBeVisible();
-    await backToLoginButton.click();
-    await expect(page).toHaveURL(`${baseURL}/login`);
-  })
-
-  // Test for password reset with an invalid temporary password
-  test('Password Reset with Invalid Temporary Password', async ({ baseURL }) => {
-
-    const signUpPage = new SignUpPage(page, baseURL);
-
-    // Navigate to reset password page with invalid email
-    await signUpPage.navigateResetPassword(newEmail);
-
-    // Fill in the invalid temporary password and submit
-    const resetPasswordtempPassword = await signUpPage.resetPasswordtempPassword();
-    const resetPasswordnewPassword = await signUpPage.resetPasswordnewPassword();
-    const resetPasswordSubmit = await signUpPage.resetPasswordSubmit();
-
-    await resetPasswordtempPassword.fill(ValidTestData.Password);
-    await resetPasswordnewPassword.fill(ValidTestData.newPassword);
-    await resetPasswordSubmit.click();
-
-    // Verify that the error message is shown
-    const resetPasswordsuccesserrormsg = await signUpPage.resetPasswordsuccesserrormsg();
-    await expect(resetPasswordsuccesserrormsg).toBeVisible();
-    await expect(resetPasswordsuccesserrormsg).toHaveText('Unable to complete this request due to an issue. Try again later.');
-  })
 });
